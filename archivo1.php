f1e36d2e28107586064c4d8c2858e95239871088
romanmussi <romanmussi@gmail.com>
2013-02-24 00:17:02 -0300

    Quitando espacios en lnx

--- archivo1.php
+++ archivo1.php
@@ -1,13 +1,9 @@
 <?php
 
 echo "Hola!!";
-
 echo "Nuevo texto agregado desde eee w7.";
-
 echo "Nuevo desde eee linux";
-
 echo "Otro nuevo desde eee w7";
-
 echo "Y finalmente otro nuevo desde eee linux";
 
 ?>
