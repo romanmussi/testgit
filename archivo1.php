478786be02d7de9f386aa02347f292a112bb95e1
romanmussi <romanmussi@gmail.com>
2013-02-25 01:04:02 -0300

    Comento código, agrego, w7

--- archivo1.php
+++ archivo1.php
@@ -4,8 +4,11 @@ echo "Hola!!";
 echo "Nuevo texto agregado desde eee w7.";
 echo "Nuevo desde eee linux";
 
-echo "Inserto linea 1";
-echo "Inserto linea 2";
+//echo "Inserto linea 1";
+//echo "Inserto linea 2";
+
+echo "Otra linea 1";
+echo "Otra linea 2";
 
 echo "Otro nuevo desde eee w7";
 echo "Y finalmente otro nuevo desde eee linux";
